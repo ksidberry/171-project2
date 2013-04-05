4ae68a6f03c40b9f7df138c158e95f5a9e4690a8
Kendall Sidberry <ksidberry16@gmail.com>
2013-04-05 10:37:48 -0400

    edited the cvs stuffs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
 window.onload = function() {	
 	// activate tooltips
 	$('.tool-info').tooltip();
-	
+
 }
+
+function buildChoropleth() {}
+
+function buildSymbol () {}
+
+function buildScatter() {}
+
+function buildTweets() {}
+
+function buildMetrics() {}
+
+function addDonut() {}
